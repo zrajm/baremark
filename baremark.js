a9aa9602f37bffbecf2352b2055bdc8437dbe6cf
zrajm <zrajm@klingonska.org>
2022-03-28 03:03:55 +0200

    Baremark (+326 bytes): Some improvements + tests

--- baremark.js
+++ baremark.js
@@ -1,29 +1,27 @@
 ((w,n)=>{
-	let esc=x=>x.replace(/\&/g,'&amp;').replace(/['#<>`*~_=:"!\[\]()\n\t-]/g,m=>`&#${m.charCodeAt(0)};`),
-	r=[
+	let esc=x=>x.replace(/\&/g,'&amp;').replace(/['\\#<>`*~_=:"!\[\]()\n\t-]/g,m=>`&#${m.charCodeAt(0)};`),r=[
 		[/\r\n?/g,'\n'],
-		[/\n\s*```\n([^]*?)\n\s*```\s*\n/g,(_,m)=>`<pre>${esc(m)}</pre>`],
-		[/`(.*?)`/g,(_,m)=>`<code>${esc(m)}</code>`],
-		[/\n\s*(#+)(.*?)/g,(_,m,n)=>`<h${m.length}>${n.trim()}</h${m.length}>`],
-		[/\n\s*(.*?)\n={3,}\n/g,'\n<h1>$1</h1>\n'],
-		[/\n\s*(.*?)\n-{3,}\n/g,'\n<h2>$1</h2>\n'],
-		[/___(.*?)___/g,'<u>$1</u>'],
-		[/(\*\*|__)(.*?)\1/g,'<strong>$2</strong>'],
-		[/(\*|_)(.*?)\1/g,'<em>$2</em>'],
-		[/~~(.*?)~~/g,'<del>$1</del>'],
-		[/:"(.*?)":/g,'<q>$1</q>'],
+		[/\n\n```\n([^]*?)\n```(?=\n\n)/g,(_,m)=>`\n\n<pre>${esc(m)}</pre>`],
+		[/(`+)((\n?.+?)*?(\n?[^`\n]|.\n))\1(?!`)/g,(_,m,n)=>`<code>${esc(n.replace(/^(\s)(.*)\1$/,'$2'))}</code>`],
+		[/\\[\x21-\x2f:;<=>?@[\\\]^_`{|}~\n]/g,m=>(x=>x==10?'<br>':`&#${x};`)(m.charCodeAt(1))],
+		[/\n\n([-_*][^\S\n]*){3,}(?=\n\n)/g,'\n\n<hr>'],
+		[/\n\n(#{1,6})[^\S\n]+(\S.*?)([^\S\n]+#+)?(?=\n\n)/g,(_,m,n)=>`\n\n<h${m.length}>${n.trim()}</h${m.length}>`],
+		[/\n\n(.+(\n.+)*)\n=+(?=\n\n)/g,'\n\n<h1>$1</h1>'],
+		[/\n\n(.+(\n.+)*)\n-+(?=\n\n)/g,'\n\n<h2>$1</h2>'],
+		[/\n\>[^\S\n]*(.*)/g,'\n\n<blockquote>\n\n$1\n\n</blockquote>\n'],
+		[/\n\n<\/(blockquote)>\n\n\n<\1>\n\n/g,'\n'],
+		[/\n[-+*][^\S\n]+(.*)/g,'\n\n<ul><li>$1</li></ul>\n'],
+		[/\n\d+[.)][^\S\n]*(.*)/g,'\n\n<ol><li>$1</li></ol>\n'],
+		[/<\/([ou]l)>\n\n\n<\1>/g,''],
+		[/___(\n?(.+\n)*?.+?\n?)___/g,'<u>$1</u>'],
+		[/(\*\*|__)(\n?(.+\n)*?.+?\n?)\1/g,'<strong>$2</strong>'],
+		[/([*_])(?!\1)(\n?(.+\n)*?.+?\n?)\1/g,'<em>$2</em>'],
+		[/~~(\n?(.+\n)*?.+?\n?)~~/g,'<del>$1</del>'],
+		[/:"(\n?(.+\n)*?.+?\n?)":/g,'<q>$1</q>'],
 		[/\!\[([^\[]+?)\]\s*\(([^\)]+?)\)/g,'<img src="$2" alt="$1">'],
 		[/\[([^\[]+?)\]\s*\(([^\)]+?)\)/g,'<a href="$2">$1</a>'],
-		[/\n\s*(\*|\-)\s*([^\n]*)/g,'\n<ul><li>$2</li></ul>'],
-		[/\n\s*\d+\.\s*([^\n]*)/g,'\n<ol><li>$1</li></ol>'],
-		[/\n\s*(\>|&gt;)\s*([^\n]*)/g,'\n<blockquote>$2</blockquote>'],
-		[/<\/(ul|ol|blockquote)>\s*<\1>/g,' '],
-		[/\n\s*\*{5,}\s*\n/g,'\n<hr>'],
-		[/\n{3,}/g,'\n\n'],
-		[/\n([^\n]+)\n/g,(_,m)=>{m=m.trim();return/^\<\/?(ul|ol|bl|h\d|p).*/.test(m.slice(0,9))?m:`<p>${m}</p>`}],
-		[/>\s+</g,'><']
-	]
+		[/\n\n(.+(\n.+)*)(?=\n\n)/g,(w,m)=>/^\<(\/|blockquote|h\d|hr|li|ol|ul|p|pre|table)\b/.test(m)?w:`\n\n<p>${m}</p>`]]
 	w[n]={
 		addRule:(p,s)=>r.push([RegExp(p,'g'),s]),
-		render:x=>r.reduce((x,[p,s])=>x.replace(p,s),`\n${x}\n`)}
+		render:x=>r.reduce((x,[p,s])=>x.replace(p,s),`\n\n${x}\n\n`).trim()}
 })(self,'Baremark')
