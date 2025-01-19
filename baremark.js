90175bf45dc3dd27e57fccc279f65560b75c3ab3
zrajm <zrajm@klingonska.org>
2025-01-19 15:18:00 +0100

    Baremark (±0 bytes): Run link/image rule earlier

--- baremark.js
+++ baremark.js
@@ -5,6 +5,8 @@
 		[/([^\\])(?<!\\)(`+)(X([^`\n\\]|.\n))\2(?!`)/,(_,p,m,n)=>p+`<tt>${e(n.replace(/^(\s)(.*)\1$/,'$2'))}</tt>`],
 		[/\\[\x21-\x2f:;<=>?@[\\\]^_`{|}~\n]/,m=>m=='\\\n'?'<br>':`&#${m.charCodeAt(1)};`],
 		[/\n\[X\]: +(?:<X>|((?!<)\S+))(?: +(?:'X'|"([^"]*)"|\(([^)]*)\)|(\S+)))?(?=\n)/,(_,n,a,b,c,d,e,f)=>(l[s(n)]=[b??s(a),c||d||e||f],'')],
+		[/(!?)\[X\]\(X\)/,(_,q,t,u)=>q?`<img src="${s(u)}" alt="${t}">`:`<a href="${s(u)}">${t}</a>`],
+		[/(!?)\[X\](?:\[X\])?/,(w,q,t,r)=>(r=l[s(r||t)],r?q?`<img src="${r[0]}" title="${r[1]||''}" alt="${t}">`:`<a href="${r[0]}" title="${r[1]||''}">${t}</a>`:w)],
 		[/\n\n([-_*]) *(\1 *){2,}(?=\n\n)/,'\n\n<hr>'],
 		[/\n\n(#{1,6}) +(\S.*?)( +#+)?(?=\n\n)/,(_,i,n)=>`\n\n<h${i=i.length}>${n}</h${i}>`],
 		[/\n(.+?(?:\n.+?)*?)\n(?:(=+)|-+)(?=\n)/,(_,x,i)=>`\n<h${i=i?1:2}>${x}</h${i}>\n`],
@@ -18,8 +20,6 @@
 		[/([*_])(?!\1)X\1/,'<i>$2</i>'],
 		[/~~X~~/,'<s>$1</s>'],
 		[/:"X":/,'<q>$1</q>'],
-		[/(!?)\[X\]\(X\)/,(_,q,t,u)=>q?`<img src="${s(u)}" alt="${t}">`:`<a href="${s(u)}">${t}</a>`],
-		[/(!?)\[X\](?:\[X\])?/,(w,q,t,r)=>(r=l[s(r||t)],r?q?`<img src="${r[0]}" title="${r[1]||''}" alt="${t}">`:`<a href="${r[0]}" title="${r[1]||''}">${t}</a>`:w)],
 		[/\n\n(.+(\n.+)*)(?=\n\n)/,(w,m)=>/^<(\/|address|article|aside|blockquote|details|div|[dou]l|fieldset|fig(caption|ure)|footer|form|h\d|header|hgroup|hr|main|menu|nav|p|pre|(no)?script|search|section|style|table)\b/.test(m)?w:`\n\n<p>${m}</p>`]
 	].map(([r,s])=>[RegExp(r.source.replace(/X/g,X),'g'),s])
 	w.baremark=x=>x===undefined?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim())
