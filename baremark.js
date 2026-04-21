a0db34d13482e8fdf433863782eaadb6d748dc54
zrajm <zrajm@klingonska.org>
2026-04-21 21:39:51 +0200

    Baremark (-31 bytes): Preprocess '\n' in regexes

--- baremark.js
+++ baremark.js
@@ -1,25 +1,25 @@
 (w=>{
-	let l,b=x=>x==null?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim()),m={X:'(\n?(?:.+?\n)*?.*?)',Y:'(\n?(?:[^()[\\]\n]+\n)*[^()[\\]\n]*)'},s=x=>x.trim().replace(/\s+/g,' '),q=c=>`&#${c.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(q,t,u,x='')=>(u=e(s(u),''),q?`<img src="${u}" title="${e(x,'')}" alt="${e(t,'')}">`:`<a href="${u}" title="${e(x,'')}">${t}</a>`),y=x=>(w,t,r,u)=>u?h(x,t,u):(r=l[v(r||t)],r?h(x,t,...r):w),v=x=>s(x).toLowerCase(),r=[
-		[/[ \t]*(\r\n?|\n)/,'\n'],
-		[/\n\n>(.*(\n.+)*)(?=\n\n)/,x=>`\n\n<blockquote>\n\n${b(x.replace(/\n> ?/g,'\n'))}\n\n</blockquote>\n`],
-		[/\n+```\n([^]*?)\n```\n*(?=\n)/,(_,m)=>`\n\n<pre>${e(m)}</pre>\n`],
-		[/([^\\])(`+)(X([^`\n\\]|.\n))\2(?!`)/,(_,p,m,n)=>p+`<tt>${e(n.replace(/^(\s)(.*)\1$/,'$2'))}</tt>`],
-		[/\\([\x21-\x2f:;<=>?@[\\\]^_`{|}~\n])/,(_,c)=>c=='\n'?'<br>':q(c)],
-		[/\n\[Y\]: +(?:<X>|((?!<)\S+))(?: +(?:'X'|"([^"]*)"|\(([^)]*)\)|(\S+)))?(?=\n)/,(_,n,a,b,c,d,g,f)=>(l[v(n)]??=[b??a,c||d||g||f],'')],
+	let l,b=x=>x==null?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim()),m={N:'\n',X:'(\n?(?:.+?\n)*?.*?)',Y:'(\n?(?:[^()[\\]\n]+\n)*[^()[\\]\n]*)'},s=x=>x.trim().replace(/\s+/g,' '),q=c=>`&#${c.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(q,t,u,x='')=>(u=e(s(u),''),q?`<img src="${u}" title="${e(x,'')}" alt="${e(t,'')}">`:`<a href="${u}" title="${e(x,'')}">${t}</a>`),y=x=>(w,t,r,u)=>u?h(x,t,u):(r=l[v(r||t)],r?h(x,t,...r):w),v=x=>s(x).toLowerCase(),r=[
+		[/[ \t]*(\rN?|N)/,'\n'],
+		[/NN>(.*(N.+)*)(?=NN)/,x=>`\n\n<blockquote>\n\n${b(x.replace(/\n> ?/g,'\n'))}\n\n</blockquote>\n`],
+		[/N+```N([^]*?)N```N*(?=N)/,(_,m)=>`\n\n<pre>${e(m)}</pre>\n`],
+		[/([^\\])(`+)(X([^`N\\]|.N))\2(?!`)/,(_,p,m,n)=>p+`<tt>${e(n.replace(/^(\s)(.*)\1$/,'$2'))}</tt>`],
+		[/\\([\x21-\x2f:;<=>?@[\\\]^_`{|}~N])/,(_,c)=>c=='\n'?'<br>':q(c)],
+		[/N\[Y\]: +(?:<X>|((?!<)\S+))(?: +(?:'X'|"([^"]*)"|\(([^)]*)\)|(\S+)))?(?=N)/,(_,n,a,b,c,d,g,f)=>(l[v(n)]??=[b??a,c||d||g||f],'')],
 		[/!\[Y\](?:\[Y\]|\(Y\))?/,y(1)],
 		[/\[Y\](?:\[Y\]|\(Y\))?/,y()],
-		[/\n\n([-_*]) *(\1 *){2,}(?=\n\n)/,'\n\n<hr>'],
-		[/\n\n(#{1,6}) +(\S.*?)( +#+)?(?=\n\n)/,(_,i,n)=>`\n\n<h${i=i.length}>${n}</h${i}>`],
-		[/\n(.+?(?:\n.+?)*?)\n(?:(=+)|-+)(?=\n)/,(_,x,i)=>`\n<h${i=i?1:2}>${x}</h${i}>\n`],
-		[/\n(?:[-+*]|(\d+)[.)]) +(.+(\n .+)*)/,(_,i,c)=>`\n\n<${i=i?'ol':'ul'}><li>${c}</li></${i}>\n`],
-		[/<\/(ol|ul)>\n\n\n<\1>/,''],
+		[/NN([-_*]) *(\1 *){2,}(?=NN)/,'\n\n<hr>'],
+		[/NN(#{1,6}) +(\S.*?)( +#+)?(?=NN)/,(_,i,n)=>`\n\n<h${i=i.length}>${n}</h${i}>`],
+		[/N(.+?(?:N.+?)*?)N(?:(=+)|-+)(?=N)/,(_,x,i)=>`\n<h${i=i?1:2}>${x}</h${i}>\n`],
+		[/N(?:[-+*]|(\d+)[.)]) +(.+(N .+)*)/,(_,i,c)=>`\n\n<${i=i?'ol':'ul'}><li>${c}</li></${i}>\n`],
+		[/<\/(ol|ul)>NNN<\1>/,''],
 		[/___X___/,'<u>$1</u>'],
 		[/(\*\*|__)X\1/,'<b>$2</b>'],
 		[/([*_])(?!\1)X\1/,'<i>$2</i>'],
 		[/~~X~~/,'<s>$1</s>'],
 		[/:"X":/,'<q>$1</q>'],
-		[/\n\n(.+(\n.+)*)(?=\n\n)/,(w,m)=>/<\/?(address|article|aside|blockquote|details|div|[dou]l|fieldset|fig(caption|ure)|footer|form|h[1-6r]|header|hgroup|main|menu|nav|p|pre|(no)?script|search|section|style|table)\b/i.test(m)?w:`\n\n<p>${m}</p>`]
-	].map(([r,s])=>[RegExp(r.source.replace(/[XY]/g,w=>m[w]),'g'),s])
+		[/NN(.+(N.+)*)(?=NN)/,(w,m)=>/<\/?(address|article|aside|blockquote|details|div|[dou]l|fieldset|fig(caption|ure)|footer|form|h[1-6r]|header|hgroup|main|menu|nav|p|pre|(no)?script|search|section|style|table)\b/i.test(m)?w:`\n\n<p>${m}</p>`]
+	].map(([r,s])=>[RegExp(r.source.replace(/[NXY]/g,w=>m[w]),'g'),s])
 	w.baremark=b
 	w.baremark.escape=e
 })(self)
