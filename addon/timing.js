ba276911f7d4d35514e7c1e153480357934d2870
zrajm <zrajm@klingonska.org>
2026-05-19 15:56:02 +0200

    Fixed addon 'timing.js'

--- addon/timing.js
+++ addon/timing.js
@@ -5,11 +5,14 @@
 
 import './baremark.js'
 
-self.baremark = (baremark => (...x) => {     // baremark() wrapper
-  if (x.length === 0) { return baremark() }
-  const ms = performance.now(), result = baremark(...x)
-  console.log(`Baremark ran in: ${performance.now() - ms} ms`)
-  return result
-})(self.baremark)
+// Wrap baremark() and any properties it might have (e.g. baremark.meta).
+self.baremark = new Proxy(self.baremark, {
+  apply(orgFunc, thisArg, args) {
+    if (args.length === 0) { return orgFunc.call(thisArg) }
+    const ms = performance.now(), result = orgFunc.apply(thisArg, args)
+    console.log(`Baremark ran in: ${performance.now() - ms} ms`)
+    return result
+  }
+});
 
 //[eof]
