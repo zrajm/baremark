7ad9377debd27000ea10c8a8abfa1e0b20a7fc68
zrajm <zrajm@klingonska.org>
2026-05-21 05:49:31 +0200

    Baremark (-4 bytes): Paragraph regex function

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ featurefulness.)
 * [Test suite]
 * [Github repo]
 
-It is currently 1937 bytes in size *before* minification and zipping!
+It is currently 1933 bytes in size *before* minification and zipping!
 
 
 <toc heading=Contents class=toc>
--- baremarkx.js
+++ baremarkx.js
@@ -1,5 +1,5 @@
 (w=>{
-	let l,b=(x,p=r[1])=>p.reduce((a,r)=>a.replace(...r),x),m={N:'\n',X:'(\n?(?:.+?\n)*?.*?)',Y:'(\n?(?:[^()[\\]\n]+\n)*[^()[\\]\n]*)'},s=x=>x.trim().replace(/\s+/g,' '),q=c=>`&#${c.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(q,t,u,x='')=>(u=e(s(u),''),x=e(x,''),q?`<img src="${u}" title="${x}" alt="${e(t,'')}">`:`<a href="${u}" title="${x}">${t}</a>`),y=x=>(w,t,r,u)=>u?h(x,t,u):(r=l[v(r||t)],r?h(x,t,...r):w),v=x=>s(x).toLowerCase(),r=[[
+	let l,j=x=>`(\n?(?:${x}+?\n)*?${x}*?)`,b=(x,p=r[1])=>p.reduce((a,r)=>a.replace(...r),x),m={N:'\n',X:j('.'),Y:j('[^()[\\]\n]')},s=x=>x.trim().replace(/\s+/g,' '),q=c=>`&#${c.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(q,t,u,x='')=>(u=e(s(u),''),x=e(x,''),q?`<img src="${u}" title="${x}" alt="${e(t,'')}">`:`<a href="${u}" title="${x}">${t}</a>`),y=x=>(w,t,r,u)=>u?h(x,t,u):(r=l[v(r||t)],r?h(x,t,...r):w),v=x=>s(x).toLowerCase(),r=[[
 		[/[ \t]*(\rN?|N)/,'\n'],
 		[/NN>(.*(N.+)*)(?=NN)/,x=>`\n\n<blockquote>${b(x.replace(/^> ?/gm,'')+'\n\n',r[0])}</blockquote>\n`],
 		[/N+```N([^]*?)N```N*(?=N)/,(_,m)=>`\n\n<pre>${e(m)}</pre>\n`],
