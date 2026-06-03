e313a899db91f41fca0967b115a1d250191a58e8
zrajm <zrajm@klingonska.org>
2026-06-03 18:30:31 +0200

    Baremark (+9 bytes): Moved regex 'macro' into function

--- baremark.js
+++ baremark.js
@@ -1,5 +1,5 @@
 (w=>{
-	let l,t=x=>`((?:${x}|\n(?!\n))*?)`,b=x=>x==null?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim()),m={N:'\n',X:t('.'),Y:t('[^()[\\]\n]')},s=x=>x.trim().replace(/\s+/g,' '),q=x=>`&#${x.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(z,x,u,a='')=>(u=e(s(u),''),a=e(a,''),z?`<img src="${u}" title="${a}" alt="${e(x,'')}">`:`<a href="${u}" title="${a}">${x}</a>`),y=z=>(w,x,r,u)=>u?h(z,x,u):(r=l[n(r||x)],r?h(z,x,...r):w),n=x=>s(x).toLowerCase(),r=[
+	let l,t=x=>`((?:${x}|\n(?!\n))*?)`,b=x=>x==null?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim()),m=r=>RegExp(r.source.replace(/[NXY]/g,x=>({N:'\n',X:t('.'),Y:t('[^()[\\]\n]')}[x])),'gi'),s=x=>x.trim().replace(/\s+/g,' '),q=x=>`&#${x.charCodeAt()};`,e=(x,r='&#|')=>x.replace(RegExp(`[${r}'\\\\<>\`*~_=:"![\\]()\n\t-]`,'g'),q),h=(z,x,u,a='')=>(u=e(s(u),''),a=e(a,''),z?`<img src="${u}" title="${a}" alt="${e(x,'')}">`:`<a href="${u}" title="${a}">${x}</a>`),y=z=>(w,x,r,u)=>u?h(z,x,u):(r=l[n(r||x)],r?h(z,x,...r):w),n=x=>s(x).toLowerCase(),r=[
 		[/[ \t]*(\rN?|N)/,'\n'],
 		[/NN>(.*(N.+)*)(?=NN)/,x=>`\n\n<blockquote>\n\n${b(x.replace(/^> ?/gm,''))}\n\n</blockquote>\n`],
 		[/N+(([~`])\2\2+) *([^~`N]*)N(?:([^]*?)N)?\1\2*N*(?=N)/,(_,a,b,c='',x='')=>`\n\n<pre class="${c}">${e(x)}</pre>\n`],
@@ -19,6 +19,6 @@
 		[/~~X~~/,'<s>$1</s>'],
 		[/:"X":/,'<q>$1</q>'],
 		[/NN(.+(N.+)*)(?=NN)/,(w,x)=>/<\/?(address|article|aside|blockquote|details|div|[dou]l|fieldset|fig(caption|ure)|footer|form|h[1-6r]|header|hgroup|main|menu|nav|p|pre|(no)?script|search|section|style|table)\b/i.test(x)?w:`\n\n<p>${x}</p>`]
-	].map(([r,s])=>[RegExp(r.source.replace(/[NXY]/g,x=>m[x]),'g'),s])
+	].map(([r,s])=>[m(r),s])
 	w.baremark=b;b.escape=e
 })(self)
